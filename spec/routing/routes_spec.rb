8f5afd9b659f81b91d73bcd34c66f13c9aefaccf
Jose Torres <torres@randomprimate.com>
2014-09-18 21:37:12 -0600

    Adding some tests

---
+++ spec/routing/routes_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'landing site' do
+
+  describe 'routing' do
+
+    it 'routes to #index' do
+      expect(:get => '/articles').to route_to('articles#index')
+    end
+
+    it 'routes to #new' do
+      expect(:get => '/articles/new').to route_to('articles#new')
+    end
+
+    it 'routes to #show' do
+      expect(:get => '/articles/1').to route_to('articles#show', :id => '1')
+    end
+
+  end
+end
