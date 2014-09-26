9aec260f5af1faf9f1b41cb2c7f77540ed1d0517
Jose Torres <torres@randomprimate.com>
2014-09-25 21:20:54 -0600

    Flash messages.

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -1,13 +1,13 @@
 describe 'the login process', :type => :feature do
   before(:each) do
-    @user = Factory.build(:user)
+    user = FactoryGirl.build(:user)
   end
 
   it 'signs me in' do
     visit '/users/sign_in'
     within('#new_user') do
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
     end
     click_button 'Submit'
     expect(page).to have_content 'Success'
