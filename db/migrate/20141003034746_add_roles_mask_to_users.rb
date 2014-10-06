b561eb0a2af3825dd50c9187214c78f429b17b69
Jose Torres <torres@randomprimate.com>
2014-10-05 22:18:46 -0600

    Updated gems

--- db/migrate/20141003034746_add_roles_mask_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddRolesMaskToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :role_id, :integer
-    add_column :users, :roles_mask, :integer
-  end
-end
--- db/migrate/20141003051522_remove_colfrom_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColfromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :role_id
-    remove_column :users, :roles_mask
-  end
-end
